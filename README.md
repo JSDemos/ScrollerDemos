dac7e91462256413c97e785944034555767ebb30
J.D.Song <juude.song@gmail.com>
2014-10-13 01:04:23 +0800

    scroll using translationX
    scroll using translationX

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-ScrollerDemos
-=============
+scrollerDemos
+===
 
-android scroller demods
+####3 ways to scroll a view
+1. `setTranslationX/setTranslationY` 
+2. `scrollTo`
+3. `setLeft/setRight`
+
+####How to implement ScrollView
+
+####How to implement RecyclerView
---
+++ src/com/juude/scrollerdemos/ScrollerDemoView.java
@@ -0,0 +1,47 @@
+package com.juude.scrollerdemos;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+public class ScrollerDemoView extends View{
+
+	private static final String TAG = "ScrollerDemoView";
+	private float mLastTouchPos;
+	private int mScaledTouchSlop;
+	private int mTranslationX;
+	public ScrollerDemoView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mScaledTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+		mTranslationX = 0;
+	}
+
+	@SuppressLint("NewApi") @Override
+	public boolean onTouchEvent(MotionEvent event) {
+		final int action = event.getAction();
+		switch(action) {
+		case MotionEvent.ACTION_DOWN:
+			mLastTouchPos =  event.getRawX();
+			Log.d(TAG, "ACTION_DOWN"  + mLastTouchPos);
+			break;
+		case MotionEvent.ACTION_MOVE:
+			float scrollPos =  event.getRawX();
+			Log.d(TAG, "ACTION_MOVE"  + scrollPos + "  mScaledTouchSlop" + mScaledTouchSlop);
+			if(Math.abs(mLastTouchPos- scrollPos) > mScaledTouchSlop / 10) {
+				mTranslationX += (int)(scrollPos - mLastTouchPos);
+				setTranslationX(mTranslationX);
+				Log.d(TAG, "ACTION_MOVE"  + (mLastTouchPos - scrollPos));
+				mLastTouchPos = scrollPos;
+			}
+			break;
+		}
+		return true;
+	}
+	
+	
+
+}
